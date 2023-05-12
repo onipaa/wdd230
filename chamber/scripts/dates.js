c723dc79c24d3db32e1c1f385ae99d1d9b7ecd46
John M Harper <john.maurice.harper@gmail.com>
2023-05-12 17:43:11 -0600

    Almost got the menu... but it's wonky (big space to the left).

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -7,9 +7,19 @@ const mySuperDuperSeconds = ('0' + mySuperDuperDate.getSeconds()).slice(-2);
 /* 
      I keep getting pesky null object errors. So, I'll make the object first 
      then assign values to them.
-*/
-document.createElement('copyrightYear')
-document.createElement('lastUpdatedDate')
 
-document.getElementById('copyrightYear').innerHTML = mySuperDuperYear
-document.getElementById('lastUpdatedDate').innerHTML = `${mySuperDuperDate.getDate()}/${mySuperDuperDate.getMonth()}/${mySuperDuperDate.getFullYear()} ${mySuperDuperHours}:${mySuperDuperMinutes}:${mySuperDuperSeconds}`
+     I needed to create an instance of a date and then assign that not-null value
+     to my datetime. I also wanted to match the format that Brother Blazzard had
+     in his example Dayofweek Month, Day-numeric, Year-numeric. It looks like
+     dateStyle: "medium" comes super close.
+*/
+
+
+const options = {
+     weekday: "long",
+     month: "long",
+     year: "numeric",
+     day: "2-digit"
+}
+
+document.getElementById("datetime").innerHTML = mySuperDuperDate.toLocaleString("en-US", options);
