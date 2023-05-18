35fb6d93354378df1a56024f2b500addde4df786
John M Harper <john.maurice.harper@gmail.com>
2023-05-18 08:22:07 -0600

    getting there

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,25 +1,12 @@
 const mySuperDuperDate = new Date(document.lastModified);
-const mySuperDuperYear = mySuperDuperDate.getFullYear();
-const mySuperDuperHours = ('0' + mySuperDuperDate.getHours()).slice(-2);
-const mySuperDuperMinutes = ('0' + mySuperDuperDate.getMinutes()).slice(-2);
-const mySuperDuperSeconds = ('0' + mySuperDuperDate.getSeconds()).slice(-2);
 
-/* 
-     I keep getting pesky null object errors. So, I'll make the object first 
-     then assign values to them.
-
-     I needed to create an instance of a date and then assign that not-null value
-     to my datetime. I also wanted to match the format that Brother Blazzard had
-     in his example Dayofweek Month, Day-numeric, Year-numeric. It looks like
-     dateStyle: "medium" comes super close.
-*/
-
-
-const options = {
+const opt_almost_full = {
      weekday: "long",
      month: "long",
      year: "numeric",
      day: "2-digit"
 }
 
-document.getElementById("datetime").innerHTML = mySuperDuperDate.toLocaleString("en-US", options);
+document.getElementById("datetime").innerHTML = mySuperDuperDate.toLocaleString("en-US", opt_almost_full);
+document.getElementById("copyrightYear").innerHTML = mySuperDuperDate.getFullYear();
+document.getElementById("lastUpdateDate").innerHTML = mySuperDuperDate.toLocaleString("en-US", opt_almost_full);
