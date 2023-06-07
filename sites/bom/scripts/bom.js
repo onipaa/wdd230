da3f6ce6f8e947ee5f7f64e597fe198c5c396e34
John M Harper <john.maurice.harper@gmail.com>
2023-06-06 21:00:52 -0600

    sprinting to catch up.

---
+++ sites/bom/scripts/bom.js
@@ -0,0 +1,6 @@
+const body = document.body
+const div = document.querySelector("div")
+const input = document.querySelector("#input")
+const button = document.querySelector("#button")
+const list = document.querySelector("#list")
+
