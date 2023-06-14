a85e6bcb4936ba7381d35921cb41f75a5ab7fffd
John M Harper <john.maurice.harper@gmail.com>
2023-06-14 15:48:40 -0600

    Ok, got the lazy loader working two ways.

--- sites/lazy/scripts/lazy.js
+++ sites/lazy/scripts/lazy.js
@@ -1,36 +1,50 @@
-// According to Kevin P:
-// when we use square brackets, the js is looking for the attribute "data-src"
-// inside the css file.
-const images = document.querySelectorAll("[data-src]")
-
-
-
-// make a new image Observer object
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-
-    img.src = src;
-}
-
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 100px 0px"
-};
-
-const imgObserver = new IntersectionObserver((entries,imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imagesObserver.unobserve(entry.target);
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyloadImages;
+  
+    if ("IntersectionObserver" in window) {
+      lazyloadImages = document.querySelectorAll(".lazy");
+      var imageObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+          if (entry.isIntersecting) {
+            var image = entry.target;
+            image.src = image.dataset.src;
+            image.classList.remove("lazy");
+            imageObserver.unobserve(image);
+          }
+        });
+      });
+  
+      lazyloadImages.forEach(function (image) {
+        imageObserver.observe(image);
+      });
+    } else {
+      var lazyloadThrottleTimeout;
+      lazyloadImages = document.querySelectorAll(".lazy");
+  
+      function lazyload() {
+        if (lazyloadThrottleTimeout) {
+          clearTimeout(lazyloadThrottleTimeout);
         }
-    })
-}, imgOptions)
-
-images.forEach(img => {
-    imgObserver.observe(image);
-}
+  
+        lazyloadThrottleTimeout = setTimeout(function () {
+          var scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function (img) {
+            if (img.offsetTop < window.innerHeight + scrollTop) {
+              img.src = img.dataset.src;
+              img.classList.remove("lazy");
+            }
+          });
+          if (lazyloadImages.length == 0) {
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+        }, 20);
+      }
+  
+      document.addEventListener("scroll", lazyload);
+      window.addEventListener("resize", lazyload);
+      window.addEventListener("orientationChange", lazyload);
+    }
+  });
+  
