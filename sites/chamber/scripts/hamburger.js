0dfa794cbfc5fa479ab9c9b78cfae609ec608d02
John M Harper <john.maurice.harper@gmail.com>
2023-06-21 05:31:34 -0600

    asdf

---
+++ sites/chamber/scripts/hamburger.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var hamburger = document.getElementById('hamburger');
+    var menu = document.getElementById('menu');
+    hamburger.classList.toggle('open');
+    menu.classList.toggle('open');
+  }
