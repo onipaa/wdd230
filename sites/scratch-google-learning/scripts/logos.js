20dd6e03cdd2fa97388966dc1527836adf8fb973
John M Harper <john.maurice.harper@gmail.com>
2023-06-26 19:43:35 -0600

    just display the logo randomly.

--- sites/scratch-google-learning/scripts/logos.js
+++ sites/scratch-google-learning/scripts/logos.js
@@ -12,47 +12,12 @@ window.onload = function() {
       .then(data => {
         const randomIndex = Math.floor(Math.random() * data.length); 
         const randomMember = data[randomIndex];
-  
         const logo = randomMember.logos;
-        const companyName = randomMember.company_name;
-        const contact = randomMember.contacts[0];
-        const contactEmail = contact.email;
-        const contactName = contact.name;
-        const contactPhone = contact.cell_phone;
-        const slogan = randomMember.slogan;
-        const subscriptionIcon = randomMember.medals;
-  
-        const cardContainer = document.createElement('div');
-        cardContainer.className = 'card-container';
-  
-        const companyHeading = document.createElement('h2');
-        companyHeading.textContent = companyName;
-  
-        const sloganParagraph = document.createElement('p');
-        sloganParagraph.textContent = slogan;
 
         const logoImage = document.createElement('img');
         logoImage.src = `images/${logo}.svg`;
         logoImage.alt = `${companyName} Logo`; 
   
-        const contactList = document.createElement('ul');
-        const contactNameItem = document.createElement('li');
-        const contactPhoneItem = document.createElement('li');
-        const contactEmailItem = document.createElement('li');
-        contactNameItem.textContent = `Contact: ${contactName}`;
-        contactPhoneItem.textContent = `Phone: ${contactPhone}`;
-        contactEmailItem.textContent = `Email: ${contactEmail}`;
-        contactList.appendChild(contactNameItem);
-        contactList.appendChild(contactPhoneItem);
-        contactList.appendChild(contactEmailItem);
-  
-        // Append the elements to the card container
-        cardContainer.appendChild(logoImage);
-        cardContainer.appendChild(companyHeading);
-        cardContainer.appendChild(sloganParagraph);
-        cardContainer.appendChild(contactList);
-  
-        // Get the spot1 element and append the card container to it
         const spot1Div = document.getElementById('spot1');
         spot1Div.appendChild(cardContainer);
       })
