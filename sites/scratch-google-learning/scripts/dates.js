74da3da8b3e0d0bc97a1c81d2a929d28847f951e
John M Harper <john.maurice.harper@gmail.com>
2023-06-27 21:20:25 -0600

    almost done need hamby

--- sites/scratch-google-learning/scripts/dates.js
+++ sites/scratch-google-learning/scripts/dates.js
@@ -7,9 +7,15 @@ const opt_almost_full = {
   day: "2-digit"
 };
 
+const opt_just_year = {
+  year: "numeric"
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   // wait until the dom is fully loaded before trying to get the innerHTML 
   // datetime, which is throwing a blasted cannot set properties of null
   // AARRGGHH!!!
   document.getElementById("datetime").innerHTML = mySuperDuperDate.toLocaleString("en-US", opt_almost_full);
+  document.getElementById("copyrightYear").innerHTML = mySuperDuperDate.toLocaleString("en-US", opt_just_year);
+  document.getElementById("myDate").innerHTML = mySuperDuperDate.toLocaleString("en-US", opt_almost_full);
 });
--- sites/scratch-google-learning/scripts/logos.js
+++ sites/scratch-google-learning/scripts/logos.js
@@ -57,6 +57,21 @@ document.addEventListener('DOMContentLoaded', function() {
           } else {
             console.error('Element with ID "spot1" not found in the document.');
         }
+
+        const spot2Div = document.getElementById('spot2');
+        if (spot1Div) {
+            spot1Div.appendChild(cardContainer);
+            cardContainer.appendChild(logoImage);
+            cardContainer.appendChild(companyHeading);
+            cardContainer.appendChild(companyLine);
+            cardContainer.appendChild(companySlogan);
+            cardContainer.appendChild(companyContact);
+            cardContainer.appendChild(companyEmail);
+            cardContainer.appendChild(companyLink);
+            
+          } else {
+            console.error('Element with ID "spot1" not found in the document.');
+        }
         
         
         console.log(cardContainer)
@@ -67,5 +82,67 @@ document.addEventListener('DOMContentLoaded', function() {
       .catch(error => {
         console.error('Error fetching member data:', error);
       });
+
+      fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomMember = data[randomIndex];
+        const logo = randomMember.logos;
+        const companyName = randomMember.company_name;
+        const contactEmail = randomMember.contact_email;
+        const contactName = randomMember.contact_name;
+        const slogan = randomMember.slogan;
+        const companyWeb = randomMember.web_address;
+  
+        const logoImage = document.createElement('img');
+        logoImage.src = `images/${logo}`;
+        logoImage.alt = `${companyName} Logo`;
+  
+        const cardContainer = document.createElement('div');
+        cardContainer.className = 'card-container';
+  
+        const companyHeading = document.createElement('h2');
+        companyHeading.textContent = companyName;
+
+        const companyLine = document.createElement('hr');
+
+        const companySlogan = document.createElement('p');
+        companySlogan.textContent = slogan;
+
+        const companyContact = document.createElement('p');
+        companyContact.textContent = contactName;
+
+        const companyEmail = document.createElement('p');
+        companyEmail.textContent = contactEmail;
+
+        const companyLink = document.createElement('a');
+        companyLink.href = companyWeb;
+        companyLink.textContent = companyWeb;        
+
+        const spot2Div = document.getElementById('spot2');
+        if (spot2Div) {
+            spot2Div.appendChild(cardContainer);
+            cardContainer.appendChild(logoImage);
+            cardContainer.appendChild(companyHeading);
+            cardContainer.appendChild(companyLine);
+            cardContainer.appendChild(companySlogan);
+            cardContainer.appendChild(companyContact);
+            cardContainer.appendChild(companyEmail);
+            cardContainer.appendChild(companyLink);
+            
+          } else {
+            console.error('Element with ID "spot1" not found in the document.');
+        }
+                
+        //console.log(cardContainer)
+        //console.log(logoImage)
+        //console.log(companyHeading)
+        //console.log(companyEmail)
+      })
+      .catch(error => {
+        console.error('Error fetching member data:', error);
+      });
+
   });
   
