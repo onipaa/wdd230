bfcde2adc110a59e950bec2d0348f551c6ac1c8f
john.maurice.harper@gmail.com <john.maurice.harper@gmail.com>
2023-07-11 04:05:02 -0600

    afterCrash

---
+++ sites/bountiful/styles/vp-1400px.cs
@@ -0,0 +1,10 @@
+@import url('https://fonts.googleapis.com/css2?family=Fredericka+the+Great&family=Fredoka:wght@300;400;500;600;700&display=swap');
+
+@media only screen and (min-width: 1400px;) {
+    #content {
+        display: grid;
+        grid-template-columns: repeat(20, 70px);
+        grid-template-rows: repeat(20, 70px);
+        
+    }
+}
