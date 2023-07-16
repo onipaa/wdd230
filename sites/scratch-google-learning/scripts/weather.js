f40f5b37e128a831fed1496390309ac1ec7ca786
john.maurice.harper@gmail.com <john.maurice.harper@gmail.com>
2023-07-15 19:58:04 -0600

    need https for github pages to work.

--- sites/scratch-google-learning/scripts/weather.js
+++ sites/scratch-google-learning/scripts/weather.js
@@ -4,7 +4,7 @@ function getCoordinates() {
     const stateCode = 'Nova Scotia';
     const countryCode = 'CA';
 
-    const apiUrlLocation = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
+    const apiUrlLocation = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`;
 
     return fetch(apiUrlLocation)
         .then(response => response.json())
