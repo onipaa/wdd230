ed46fc4830709ce856761321e6f3af2faef2b9b1
john.maurice.harper@gmail.com <john.maurice.harper@gmail.com>
2023-07-16 00:01:18 -0600

    ok... enough for tonight.

--- sites/chamber/scripts/form.js
+++ sites/chamber/scripts/form.js
@@ -12,6 +12,7 @@ function validateInput() {
     that's it so Gene would have to explain it all the time. Oh! and so kids
     would give Gene a hard time about it.
     pattern1: starts with + [any letter combo + space] + at least one or more + end
+
     pattern2: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g
         ^               - start with
         [+]*            - matches zero or more "+" signs
@@ -22,13 +23,20 @@ function validateInput() {
         $               - end
         g               - gloal search
         s               - single line if I wanted to
-    pattern3: /^[a-zA-z0-9 \-]{7,}$/g    
+    
+    pattern3: /^[a-zA-z0-9 \-]{7,}$/ig
+        ^               - starts with
+        [a-zA-Z0-9 \-]+ - alpha numeric space or hyphen
+        {7,}            - at least 7 or more characters
+        $               - end
+        g               - global search
+        i               - ignore case
     */
 
     constNamePattern = /^[a-zA-Z ]+$/;
     constCellPattern = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g
     constTitlePattern = /^[a-zA-z0-9 \-]{7,}$/g
-    
+
 
 
     const inpGivenNamesValue = inpGivenNames.value;
@@ -44,6 +52,7 @@ function validateInput() {
 
 }
 
+// add listeners
 inpGivenNames.addEventListener('blur', validateInput);
-inpFamilyNames.addEventListener('blur', validateInput);
+
 
